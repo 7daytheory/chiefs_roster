ac41f0b6fc802ea5d55b31b5feea2ffc6dd9aad2
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-09-30 22:49:56 -0400

    Setup Card function that takes in different props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-function Card() {
+import patrickMahomes from '../src/images/patrickMahomes.webp';
+import justinReid from '../src/images/justinReid.webp';
+
+function Card(props) {
     return (
       <div>
-        <h2>Patrick Mahomes</h2>
-        <img src="images/thisimage.png"
-        alt="Patrick Mahomes"
+        <h2>{props.name}</h2>
+        <h3>{props.position}</h3>
+        <h3>{props.num}</h3>
+        <img
+          src={props.url}
+          alt={props.name}
         />
-        <p>123 456 789</p>
-        <p>height</p>
-        <p>Weight</p>
+        <p>{props.exp}</p>
+        <p>{props.age}</p>
       </div>
     )
 }
@@ -15,8 +20,23 @@ function Card() {
 function App() {
   return (
       <header>
-          <Card />
-          <Card />
+      <Card
+        url={patrickMahomes}
+        name="Patrick Mahomes"
+        position="Quarterback"
+        num="15"
+        exp="5 years"
+        age="25"
+        />
+
+        <Card
+          url={justinReid}
+          name="Justin Reid"
+          position="Safety"
+          num="30"
+          exp="3 years"
+          age="24"
+          />
       </header>
   );
 }
