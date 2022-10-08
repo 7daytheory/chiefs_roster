cc51025237051cb1a64be0a635867b9a16db04f3
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-08 14:43:17 -0400

    Mapped through chiefs json file to get all Chiefs players

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,16 +5,16 @@ function Card(props) {
     return (
       <div className="card">
         <div className="top">
-        <h2 className="name">{props.name}</h2>
+        <h2 className="name">{props.name} {props.lastName}</h2>
         <p className="position">{props.position}</p>
           <Avatar
           url={props.url}
-          alt={props.name}
+          alt={props.LastName}
           />
         </div>
         <div className="bottom">
         <h3 className="info">Player Info</h3>
-        <p className="height">Height : {props.height}</p>
+        <p className="height">Height : {props.heightFeet}'{props.heightInches}</p>
         <p className="weight">Weight : {props.weight}</p>
         <p className="age">Age : {props.age} years</p>
         <p className="exp">{props.exp} Years Experience</p>
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -3,30 +3,52 @@ import Card from '../Card/Card';
 import patrickMahomes from '../../images/patrickMahomes.webp';
 import justinReid from '../../images/justinReid.webp';
 
+import chiefs from '../../chiefs.js';
+
+function createCard(chiefs) {
+  return (
+    <Card
+    key={chiefs.PlayerID}
+    url={patrickMahomes}
+    name={chiefs.FirstName}
+    lastName={chiefs.LastName}
+    position={chiefs.Position}
+    num={chiefs.Number}
+    exp={chiefs.Experience}
+    age={chiefs.Age}
+    heightFeet={chiefs.HeightFeet}
+    heightInches={chiefs.HeightInches}
+    weight={chiefs.Weight}
+  />
+);
+}
+
 function Players() {
   return (
     <div>
-        <Card
-          url={patrickMahomes}
-          name="Patrick Mahomes"
-          position="QB"
-          num="15"
-          exp="5"
-          age="25"
-          height="6'2"
-          weight="202"
-          />
-
-        <Card
-          url={justinReid}
-          name="Justin Reid"
-          position="S"
-          num="30"
-          exp="3"
-          age="24"
-          height="6'1"
-          weight="210"
-          />
+    {chiefs.map(createCard)}
+        {// <Card
+        //   url={patrickMahomes}
+        //   name="Patrick Mahomes"
+        //   position="QB"
+        //   num="15"
+        //   exp="5"
+        //   age="25"
+        //   height="6'2"
+        //   weight="202"
+        //   />
+        //
+        // <Card
+        //   url={justinReid}
+        //   name="Justin Reid"
+        //   position="S"
+        //   num="30"
+        //   exp="3"
+        //   age="24"
+        //   height="6'1"
+        //   weight="210"
+        //   />
+      }
     </div>
   );
 }
