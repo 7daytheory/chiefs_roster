f43217ab27d5a2ab5fd6fadf03566cfac8453e25
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-10 13:42:44 -0400

    Stored json file locally, commented out for later

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Players from './components/Players/Players';
 import Header from './components/Header/Header';
-import Lastgame from './components/Lastgame/Lastgame';
+//import Lastgame from './components/Lastgame/Lastgame';
 
 function App() {
   return (
     <div>
-      <Lastgame />
       <Header />
       <Players />
     </div>
