3d696033e858cd0d9875f75d6c08fa6db4d79499
Matthew Lowe <Matthewjlowe11@gmail.com>
2022-10-10 14:12:29 -0400

    Display N/A for players that don't have numbers

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -10,7 +10,7 @@ function createCard(chiefs) {
     name={chiefs.FirstName}
     lastName={chiefs.LastName}
     position={chiefs.Position}
-    num={chiefs.Number}
+    num={chiefs.Number ? chiefs.Number : 'N/A'}
     exp={chiefs.Experience}
     age={chiefs.Age}
     heightFeet={chiefs.HeightFeet}
